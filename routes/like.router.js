b2c7fc7fc623d043b9cdc4d649abda0683f94ba9
wlduq0150 <wlduq0150@gmail.com>
2023-11-22 18:27:29 +0900

    좋아요 기능 에러 수정

--- routes/like.router.js
+++ routes/like.router.js
@@ -62,14 +62,12 @@ likeRouter.post("/comments/:commentId/like", async (req, res, next) => {
             likes -= 1;
         }
 
-        throw new Error("트랜잭션 테스트");
-
-
         // 댓글 좋아요 카운트
         await Comment.update({
             likes,
         }, {
-            where: { id: commentId }
+            where: { id: commentId },
+            transaction: t
         });
 
         // 트랜잭션 커밋
