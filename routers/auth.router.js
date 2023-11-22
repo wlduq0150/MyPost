bc30ae6085ed8987992b5b7404580efdd826cd16
Jonghyeon Choi <whd617@naver.com>
2023-11-22 17:27:10 +0900

    요청 수정

--- routers/auth.router.js
+++ routers/auth.router.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
 import bcrypt from "bcrypt";
 import db from "../models/index.js";
+import jwt from "jsonwebtoken";
 import { PASSWORD_SALT_ROUNDS } from "../constants/security.constant.js";
 const { User } = db;
 const authRouter = Router();
-
+const datePattern = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$/;
 // 회원가입 /api/auth/signup
-authRouter.post("/signup", async (req, res) => {
+authRouter.post("/signup", async (req, res, next) => {
     try {
-        const { email, name, birth, myIntro, password, passwordConfirm } = req.body;
+        const { email, name, myIntro, password, passwordConfirm, birth } = req.body;
+
         // 이메일 정보가 없는 경우
         if (!email) {
             return res.status(400).json({
@@ -31,7 +33,7 @@ authRouter.post("/signup", async (req, res) => {
             });
         }
 
-        if (!Date(birth)) {
+        if (!datePattern.test(birth)) {
             return res.status(400).json({
                 ok: false,
                 message: "생년월일을 잘못 입력하셨습니다.",
@@ -77,7 +79,7 @@ authRouter.post("/signup", async (req, res) => {
         const existedUser = await User.findOne({ where: { email } });
 
         if (existedUser) {
-            return res.status(400).json({
+            return res.status(409).json({
                 ok: false,
                 message: "이미 가입된 이메일입니다.",
             });
@@ -89,12 +91,58 @@ authRouter.post("/signup", async (req, res) => {
             await User.create({ email, name, birth, myIntro, password: hashedPassword })
         ).toJSON();
         delete newUser.password;
-        console.log({ newUser: newUser.toJSON() });
 
         return res.status(201).json({
             ok: true,
             message: "회원가입에 성공하였습니다.",
             data: newUser,
+        });
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 로그인/api/auth/signin
+authRouter.post("/signin", async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email) {
+            return res.status(400).json({
+                ok: false,
+                message: "이메일 입력이 필요합니다.",
+            });
+        }
+
+        if (!password) {
+            return res.status(400).json({
+                ok: false,
+                message: "비밀번호 입력이 필요합니다.",
+            });
+        }
+
+        const user = (await User.findOne({ where: { email } }))?.toJSON();
+        const hashedPassword = user?.password;
+        const isPasswordMatched = bcrypt.compareSync(password, hashedPassword);
+
+        const isCorretUser = user && isPasswordMatched;
+
+        if (!isCorretUser) {
+            return res.status(400).json({
+                ok: false,
+                message: "일치하지 인증 정보가 없습니다.",
+            });
+        }
+
+        jwt.sign();
+
+        const accessToken = null;
+        const refreshToken = null;
+
+        return res.status(200).json({
+            ok: true,
+            message: "회원가입에 성공하였습니다.",
+            data: { accessToken, refreshToken },
         });
     } catch (error) {
         console.error(error);
@@ -104,8 +152,5 @@ authRouter.post("/signup", async (req, res) => {
         });
     }
 });
-
-// 로그인/api/auth/signin
-authRouter.post("/signin", async (req, res) => {});
 
 export { authRouter };
