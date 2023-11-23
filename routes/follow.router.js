c21a751e080652497b5a69989370f3a5a916b816
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-11-23 15:29:48 +0900

    transaction 빠진 부분 수정

--- routes/follow.router.js
+++ routes/follow.router.js
@@ -8,7 +8,7 @@ const router = express.Router();
 // todo: 인증 추가
 // 팔로우
 router.post("/follow/:followeeId", async (req, res, next) => {
-    const userId = 1;
+    const userId = 3;
     const followeeId = +req.params.followeeId;
     if (userId === followeeId)
         return res.status(400).json({ ok: false, message: "자신을 팔로우할 수 없습니다." });
@@ -20,8 +20,8 @@ router.post("/follow/:followeeId", async (req, res, next) => {
         return res.status(400).json({ ok: false, message: "이미 팔로우한 사용자입니다." });
     try {
         await db.sequelize.transaction(async (t) => {
-            await Follow.create({ followerId: userId, followeeId });
-            await existFollowee.update({ followers: existFollowee.followers + 1 });
+            await Follow.create({ followerId: userId, followeeId }, {transaction: t});
+            await existFollowee.update({ followers: existFollowee.followers + 1 }, {transaction: t});
         });
         res.status(201).json({ ok: true, message: "해당 사용자를 팔로우했습니다." });
     } catch (e) {
@@ -32,17 +32,17 @@ router.post("/follow/:followeeId", async (req, res, next) => {
 
 // 언팔로우
 router.delete("/follow/:followeeId", async (req, res, next) => {
-    const userId = 1;
+    const userId = 3;
     const followeeId = +req.params.followeeId;
     const existFollow = await Follow.findOne({ where: { followerId: userId, followeeId } });
     if (!existFollow)
         return res.status(400).json({ ok: false, message: "팔로우하지 않은 사용자입니다." });
     try {
         await db.sequelize.transaction(async (t) => {
-            await existFollow.destroy();
-            const existFollowee = await User.findOne({ where: { id: followeeId } });
+            await existFollow.destroy({transaction: t});
+            const existFollowee = await User.findOne({ where: { id: followeeId } },{transaction: t});
             if (existFollowee)
-                await existFollowee.update({ followers: existFollowee.followers - 1 });
+                await existFollowee.update({ followers: existFollowee.followers - 1 },{transaction: t});
         });
         res.status(201).json({ ok: true, message: "해당 사용자를 언팔로우했습니다." });
     } catch (e) {
