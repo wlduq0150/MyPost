3bf60118f0e8376275bb7bb4d1348f9931a0b007
dabbler2 <117023230+dabbler2@users.noreply.github.com>
2023-11-23 15:30:52 +0900

    transaction 빠진 부분 수정

--- routes/follow.router.js
+++ routes/follow.router.js
@@ -20,13 +20,16 @@ router.post("/follow/:followeeId", async (req, res, next) => {
         return res.status(400).json({ ok: false, message: "이미 팔로우한 사용자입니다." });
     try {
         await db.sequelize.transaction(async (t) => {
-            await Follow.create({ followerId: userId, followeeId }, {transaction: t});
-            await existFollowee.update({ followers: existFollowee.followers + 1 }, {transaction: t});
+            await Follow.create({ followerId: userId, followeeId }, { transaction: t });
+            await existFollowee.update(
+                { followers: existFollowee.followers + 1 },
+                { transaction: t }
+            );
         });
         res.status(201).json({ ok: true, message: "해당 사용자를 팔로우했습니다." });
     } catch (e) {
         console.log(e);
-		next(e);
+        next(e);
     }
 });
 
@@ -39,15 +42,21 @@ router.delete("/follow/:followeeId", async (req, res, next) => {
         return res.status(400).json({ ok: false, message: "팔로우하지 않은 사용자입니다." });
     try {
         await db.sequelize.transaction(async (t) => {
-            await existFollow.destroy({transaction: t});
-            const existFollowee = await User.findOne({ where: { id: followeeId } },{transaction: t});
+            await existFollow.destroy({ transaction: t });
+            const existFollowee = await User.findOne(
+                { where: { id: followeeId } },
+                { transaction: t }
+            );
             if (existFollowee)
-                await existFollowee.update({ followers: existFollowee.followers - 1 },{transaction: t});
+                await existFollowee.update(
+                    { followers: existFollowee.followers - 1 },
+                    { transaction: t }
+                );
         });
         res.status(201).json({ ok: true, message: "해당 사용자를 언팔로우했습니다." });
     } catch (e) {
         console.log(e);
-		next(e);
+        next(e);
     }
 });
 
