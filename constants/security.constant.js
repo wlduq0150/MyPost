3964ed7c963555d4eb5fa1329dfd09c22a863e93
Jonghyeon Choi <whd617@naver.com>
2023-11-24 18:18:56 +0900

    feat/인증-Middleware-구현#4-cookie검증 방식으로 변경

--- constants/security.constant.js
+++ constants/security.constant.js
@@ -4,7 +4,7 @@ import db from "../models/index.js";
 export const PASSWORD_SALT_ROUNDS = Number.parseInt(process.env.PASSWORD_SALT_ROUNDS, 10);
 export const JWT_ACCESS_TOKEN_SECRET = process.env.JWT_ACCESS_TOKEN_SECRET;
 export const JWT_REFRESH_TOKEN_SECRET = process.env.JWT_REFRESH_TOKEN_SECRET;
-export const JWT_ACCESS_TOKEN_EXPIRES_IN = "1s";
+export const JWT_ACCESS_TOKEN_EXPIRES_IN = "12h";
 export const JWT_REFRESH_TOKEN_EXPIRES_IN = "7d";
 
 export const issueRefreshToken = async (userId) => {
